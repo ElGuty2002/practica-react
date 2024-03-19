7aa6a3b41854991366c8acc5540c6b6e105625e2
ElGuty2002 <159204239+ElGuty2002@users.noreply.github.com>
2024-03-19 00:37:32 -0500

    avance mitad
    avance de la mitad

--- tricki/src/App.js
+++ tricki/src/App.js
@@ -1,23 +1,29 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board/Board.js';
+import Square from './components/Square/Square.js';
 
-function App() {
+
+const App = () => {
+  const [turn, setTurn] = useState('X');
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [score, setScore] = useState({
+    X: 0,
+    O: 0,
+  });
+  const checkForWinner = squares => {
+    setTurn(Turn=== 'X'? 'O' : 'X');
+  }
+  const handleClick = Square => {
+    let newSquares= [...squares]
+    newSquares.splice(square, 1, turn);
+    setSquares(newSquares);
+    checkForWinner(newSquares);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Board squares={squares}/>
     </div>
   );
 }
---
+++ tricki/src/components/Square/Square.js
@@ -0,0 +1,9 @@
+import './Square.css';
+const Square = ({value}) => {
+    return (
+        <div className="square">
+
+        </div>
+    )
+}
+export default Square;
