a95219fb51a675635abd575c685a03c58f7e9800
ElGuty2002 <159204239+ElGuty2002@users.noreply.github.com>
2024-03-19 17:44:02 -0500

    tricki
    actualizado

--- tricki/src/components/Square/Square.js
+++ tricki/src/components/Square/Square.js
@@ -1,9 +1,23 @@
 import './Square.css';
-const Square = ({value}) => {
+import classNames from 'classnames';
+
+const Square = ({ value, onClick, turn, winner }) => {
+
+    const handleClick = () => {
+        (turn !== null && value === null) && onClick();
+    }
+
+    let squareClass = classNames({
+        square: true,
+        [`square--${value}`]: value !== null,
+        winner: winner,
+    });
+
     return (
-        <div className="square">
+        <div className={squareClass} onClick={() => handleClick()}>
 
         </div>
     )
 }
+
 export default Square;
